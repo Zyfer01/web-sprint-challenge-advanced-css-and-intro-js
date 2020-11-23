a49b2d7be8fe079aebf9283dcf83fdeb7c58d6cb
Richard-Cunninghamgit config --global user.email wolfpack0822@gmail.com <King Richard@DESKTOP-OGEDPOB.(none)>
2020-11-22 23:22:40 -0500

    attempted

--- index.js
+++ index.js
@@ -208,16 +208,14 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
-
+console.log(artists.name[0])
 //(2) Bio of the third artist (2nd index) in the array 
-
-
+console.log(artists.name[2])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artists.replace([8],"Vincent Van Dough");
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,8 +226,8 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(number) {
+  return (19,"Albrecht Dürer");
 }  
 
 
@@ -242,8 +240,8 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(name){
+  return artists.names
 }
 
 
@@ -257,7 +255,7 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
+function removeArtist(number){
    /*Your Code Here*/
 }
    
@@ -278,8 +276,14 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(artists){
+    addArtist.push(id: 20,
+      name: Your Name Here, 
+      years: Your Birth Year - current day,
+      genre: Web Design, 
+      nationality: Your Nationality Here
+      bio: Add 1-2 sentences (or use lorem ipsum))
+      return artists;
   }
 
   
@@ -291,8 +295,8 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(paintings){
+  return lotsOfArt(100);
 }
 
 
